0a2eaa00b3bc6bf148aca91648dd28f00ade1cb0
Brendan Chen <me@bchen.dev>
2024-03-18 12:02:39 -0700

    update card view arguments

--- OceanPlasticsDemo/CardView.swift
+++ OceanPlasticsDemo/CardView.swift
@@ -8,12 +8,33 @@
 import Foundation
 import SwiftUI
 
-struct CardView: View {
+struct CardView<Content: View>: View {
+    let imageContent: Content
+    let caption: String
+    let title: String
+    let description: String
+
+    init(caption: String, title: String, description: String, @ViewBuilder content: () -> Content) {
+        self.imageContent = content()
+        self.caption = caption
+        self.title = title
+        self.description = description
+    }
+
     var body: some View {
-        Text("Card view")
+        VStack {
+            imageContent
+            Text(caption)
+            Text(title)
+            Text(description)
+        }
     }
 }
 
 #Preview {
-    CardView()
+    CardView(caption: "Surfrider Foundation", title: "Dockweiler Beach Cleanup", description: "April 14th, 2024") {
+        Image("dockweiler")
+            .resizable()
+    }
 }
+
