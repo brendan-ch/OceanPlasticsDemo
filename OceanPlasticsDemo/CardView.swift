c071385b01792125b71a3d221b9ea57a85002c26
Brendan Chen <me@bchen.dev>
2024-03-18 11:26:22 -0700

    add CardView stub

---
+++ OceanPlasticsDemo/CardView.swift
@@ -0,0 +1,19 @@
+//
+//  CardView.swift
+//  OceanPlasticsDemo
+//
+//  Created by Brendan Chen on 2024.03.18.
+//
+
+import Foundation
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text("Card view")
+    }
+}
+
+#Preview {
+    CardView()
+}
