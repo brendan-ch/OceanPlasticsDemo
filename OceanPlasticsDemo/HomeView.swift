c5d82180b1ff90c43fa40364e6e3217e212dd93a
Brendan Chen <me@bchen.dev>
2024-03-18 11:08:43 -0700

    remove placeholder data and rename to home view

---
+++ OceanPlasticsDemo/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  OceanPlasticsDemo
+//
+//  Created by Brendan Chen on 2024.03.18.
+//
+
+import SwiftUI
+import SwiftData
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
--- OceanPlasticsDemo/OceanPlasticsDemoApp.swift
+++ OceanPlasticsDemo/OceanPlasticsDemoApp.swift
@@ -25,7 +25,7 @@ struct OceanPlasticsDemoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
         .modelContainer(sharedModelContainer)
     }
