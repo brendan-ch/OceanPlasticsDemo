c5378f95323a15443f5e3712cb817198c0536442
Brendan Chen <me@bchen.dev>
2024-03-18 11:24:22 -0700

    add EventView

---
+++ OceanPlasticsDemo/EventView.swift
@@ -0,0 +1,19 @@
+//
+//  EventView.swift
+//  OceanPlasticsDemo
+//
+//  Created by Brendan Chen on 2024.03.18.
+//
+
+import Foundation
+import SwiftUI
+
+struct EventView: View {
+    var body: some View {
+        Text("Event View")
+    }
+}
+
+#Preview {
+    EventView()
+}
