e8827dfddb427834b6d71806540b7553260d7f35
Brendan Chen <me@bchen.dev>
2024-03-18 11:23:43 -0700

    add NonprofitView

---
+++ OceanPlasticsDemo/NonprofitView.swift
@@ -0,0 +1,19 @@
+//
+//  NonprofitView.swift
+//  OceanPlasticsDemo
+//
+//  Created by Brendan Chen on 2024.03.18.
+//
+
+import Foundation
+import SwiftUI
+
+struct NonprofitView: View {
+    var body: some View {
+        Text("Nonprofit View")
+    }
+}
+
+#Preview {
+    NonprofitView()
+}
