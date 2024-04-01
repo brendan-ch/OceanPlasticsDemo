aefa14ea57961c34a403a9972f0dd5e8905f666d
Brendan Chen <me@bchen.dev>
2024-04-01 13:54:07 -0700

    add PreviewDataController singleton

---
+++ OceanPlasticsDemo/PreviewDataController.swift
@@ -0,0 +1,28 @@
+//
+//  PreviewDataController.swift
+//  OceanPlasticsDemo
+//
+//  Created by Brendan Chen on 2024.04.01.
+//
+
+import Foundation
+import SwiftData
+
+/// Singleton object for creating preview data in SwiftUI previews.
+@MainActor
+class PreviewDataController {
+    static let previewContainer: ModelContainer = {
+        let schema = Schema([
+            Event.self,
+            Nonprofit.self,
+        ])
+        
+        let options = ModelConfiguration(isStoredInMemoryOnly: true)
+        
+        let container = try! ModelContainer(for: schema, configurations: [options])
+        
+        container.mainContext.insert(Nonprofit(name: "Surfrider Foundation", following: true, about: "The Surfrider Foundation USA is a U.S. 501 grassroots non-profit environmental organization that works to protect and preserve the world's oceans, waves and beaches. It focuses on water quality, beach access, beach and surf spot preservation, and sustaining marine and coastal ecosystems. (Wikipedia)", externalResources: [], mailingAddress: "P.O. Box 73550, San Clemente, CA 92673"))
+        
+        return container
+    }()
+}
--- OceanPlasticsDemo/TabbedView.swift
+++ OceanPlasticsDemo/TabbedView.swift
@@ -30,17 +30,6 @@ struct TabbedView: View {
 }
 
 #Preview {
-    let schema = Schema([
-        Event.self,
-        Nonprofit.self,
-    ])
-    
-    let options = ModelConfiguration(isStoredInMemoryOnly: true)
-    
-    let container = try! ModelContainer(for: schema, configurations: [options])
-    
-    container.mainContext.insert(Nonprofit(name: "Surfrider Foundation", following: true, about: "The Surfrider Foundation USA is a U.S. 501 grassroots non-profit environmental organization that works to protect and preserve the world's oceans, waves and beaches. It focuses on water quality, beach access, beach and surf spot preservation, and sustaining marine and coastal ecosystems. (Wikipedia)", externalResources: [], mailingAddress: "P.O. Box 73550, San Clemente, CA 92673"))
-    
     return TabbedView()
-        .modelContainer(container)
+        .modelContainer(PreviewDataController.previewContainer)
 }
