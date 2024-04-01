32dafcdbbc1a4ef6fb0bccbe6e83493a2b5218d3
Brendan Chen <me@bchen.dev>
2024-04-01 15:11:38 -0700

    add additional beach cleanup

--- OceanPlasticsDemo/PlaceholderDataController.swift
+++ OceanPlasticsDemo/PlaceholderDataController.swift
@@ -38,6 +38,10 @@ class PlaceholderDataController {
         let dockweiler = Event(name: "Dockweiler Beach Cleanup", imageAssetName: "dockweiler", date: Date.now, externalSignupLink: URL(string: "https://surfrider.com")!, bookmarked: false, about: "Join us at one of the largest annual beach cleanups in Southern California! This event is located at Dockweiler Beach, which covers over 3.7 miles and 288 acres of beach, and is situated right next to Los Angeles International Airport. Food and amenities will be provided!", latitude: nil, longitude: nil)
         dockweiler.nonprofit = surfrider
         
+        let balboa = Event(name: "Balboa Beach Cleanup", imageAssetName: "balboa", date: Date.now, externalSignupLink: URL(string: "https://surfrider.com")!, bookmarked: false, about: "Join us at our annual Balboa Beach Cleanup! Located on the beautiful Balboa Peninsula, this event takes place over 2.5 miles of beach. Food and amenities will be provided, or stop by any of the local businesses along the peninsula!", latitude: nil, longitude: nil)
+        balboa.nonprofit = surfrider
+        
         container.mainContext.insert(dockweiler)
+        container.mainContext.insert(balboa)
     }
 }
