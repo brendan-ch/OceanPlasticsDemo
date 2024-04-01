2f22724efe1aa29e458f0703465777fce3f72adf
Brendan Chen <me@bchen.dev>
2024-04-01 13:50:51 -0700

    add preview dummy data to tabbed view

--- OceanPlasticsDemo/TabbedView.swift
+++ OceanPlasticsDemo/TabbedView.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import SwiftData
 
 struct TabbedView: View {
     var body: some View {
@@ -29,5 +30,17 @@ struct TabbedView: View {
 }
 
 #Preview {
-    TabbedView()
+    let schema = Schema([
+        Event.self,
+        Nonprofit.self,
+    ])
+    
+    let options = ModelConfiguration(isStoredInMemoryOnly: true)
+    
+    let container = try! ModelContainer(for: schema, configurations: [options])
+    
+    container.mainContext.insert(Nonprofit(name: "Surfrider Foundation", following: true, about: "The Surfrider Foundation USA is a U.S. 501 grassroots non-profit environmental organization that works to protect and preserve the world's oceans, waves and beaches. It focuses on water quality, beach access, beach and surf spot preservation, and sustaining marine and coastal ecosystems. (Wikipedia)", externalResources: [], mailingAddress: "P.O. Box 73550, San Clemente, CA 92673"))
+    
+    return TabbedView()
+        .modelContainer(container)
 }
