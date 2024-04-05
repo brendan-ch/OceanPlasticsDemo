7e7e4dfe52100bd4a93bb93f9bae998542d26d22
Brendan Chen <me@bchen.dev>
2024-04-05 12:45:40 -0700

    update demo data to include specific times

--- OceanPlasticsDemo/PlaceholderDataController.swift
+++ OceanPlasticsDemo/PlaceholderDataController.swift
@@ -28,6 +28,7 @@ class PlaceholderDataController {
     }()
     
     static func insertPlaceholderData(container: ModelContainer) {
+        // Clear the existing data without clearing schemas
         try! container.mainContext.delete(model: Nonprofit.self)
         try! container.mainContext.delete(model: Event.self)
         
@@ -35,10 +36,12 @@ class PlaceholderDataController {
         
         container.mainContext.insert(surfrider)
         
-        let dockweiler = Event(name: "Dockweiler Beach Cleanup", imageAssetName: "dockweiler", date: Date.now, externalSignupLink: URL(string: "https://surfrider.com")!, bookmarked: false, about: "Join us at one of the largest annual beach cleanups in Southern California! This event is located at Dockweiler Beach, which covers over 3.7 miles and 288 acres of beach, and is situated right next to Los Angeles International Airport. Food and amenities will be provided!", latitude: nil, longitude: nil)
+        // Create an event on April 14th, 2024 at 9:00 AM PST
+        let dockweiler = Event(name: "Dockweiler Beach Cleanup", imageAssetName: "dockweiler", date: Date(timeIntervalSince1970: TimeInterval(1713110400)), externalSignupLink: URL(string: "https://surfrider.com")!, bookmarked: false, about: "Join us at one of the largest annual beach cleanups in Southern California! This event is located at Dockweiler Beach, which covers over 3.7 miles and 288 acres of beach, and is situated right next to Los Angeles International Airport. Food and amenities will be provided!", latitude: nil, longitude: nil)
         dockweiler.nonprofit = surfrider
         
-        let balboa = Event(name: "Balboa Beach Cleanup", imageAssetName: "balboa", date: Date.now, externalSignupLink: URL(string: "https://surfrider.com")!, bookmarked: false, about: "Join us at our annual Balboa Beach Cleanup! Located on the beautiful Balboa Peninsula, this event takes place over 2.5 miles of beach. Food and amenities will be provided, or stop by any of the local businesses along the peninsula!", latitude: nil, longitude: nil)
+        // Create an event on May 5th, 2024 at 9:00 AM PST
+        let balboa = Event(name: "Balboa Beach Cleanup", imageAssetName: "balboa", date: Date(timeIntervalSince1970: TimeInterval(1714924800)), externalSignupLink: URL(string: "https://surfrider.com")!, bookmarked: false, about: "Join us at our annual Balboa Beach Cleanup! Located on the beautiful Balboa Peninsula, this event takes place over 2.5 miles of beach. Food and amenities will be provided, or stop by any of the local businesses along the peninsula!", latitude: nil, longitude: nil)
         balboa.nonprofit = surfrider
         
         container.mainContext.insert(dockweiler)
