6dc305601a66af3d114e6be65aff4c14c69621cc
Brendan Chen <me@bchen.dev>
2024-04-05 12:41:09 -0700

    implement view and query for upcoming events

--- OceanPlasticsDemo/UpcomingEventsView.swift
+++ OceanPlasticsDemo/UpcomingEventsView.swift
@@ -7,13 +7,39 @@
 
 import Foundation
 import SwiftUI
+import SwiftData
 
 struct UpcomingEventsView: View {
+    @Query(sort: \Event.date) var events: [Event]
+    
     var body: some View {
-        Text("Upcoming Events View")
+        List {
+            ForEach(events) { event in
+                ZStack {
+                    NavigationLink {
+                        EventView()
+                    } label: {
+                        EmptyView()
+                    }
+                    .opacity(0.0)
+                    CardView(caption: event.nonprofit?.name ?? "", title: event.name, description: event.date.formatted()) {
+                        Image(event.imageAssetName)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(height: 200)
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                            .clipped()
+                    }
+                }
+                .listRowInsets(EdgeInsets())
+            }
+        }
+        .listStyle(.plain)
+        .navigationTitle("Events")
     }
 }
 
 #Preview {
     UpcomingEventsView()
+        .modelContainer(PlaceholderDataController.previewContainer)
 }
